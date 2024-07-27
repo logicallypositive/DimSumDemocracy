64aca56c151432555da5152d31d0dde88babd928
logically positive <brianizquierdo0229@gmail.com>
2024-07-27 18:33:59 -0400

    updates to structs

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,36 @@ use std::io::BufReader;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
+struct Ingredient {
+    name: String,
+    quantity: f32,
+    unit: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
 struct Recipe {
+    ingredients: Vec<Ingredient>,
+    steps: Vec<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Food {
     id: u32,
     name: String,
     vegan: bool,
-    recency: String
+    recency: String,
+    nutrition_value: f32,
+    yummy_value: f32,
+    affordability: f32,
+    preparation_time: u32,
+    serving_size: u32,
+    cuisine: String,
+    spiciness_level: u32,
+    caloric_information: u32,
+    shelf_life: u32,
+    meal_prep_friendly: bool,
+    serving_suggestions: Vec<String>,
+    recipe: Recipe,
 }
 
 fn main() -> std::io::Result<()> {
@@ -16,9 +41,9 @@ fn main() -> std::io::Result<()> {
     let file = File::open("data/recipes.json")?;
     let reader = BufReader::new(file);
 
-    let json_data: Vec<Recipe> = serde_json::from_reader(reader)?;
+    let json_data: Vec<Food> = serde_json::from_reader(reader)?;
 
-    let temp: Recipe = chooser(&json_data);
+    let temp: Food = chooser(&json_data);
 
     for recipe in json_data.iter() {
         println!("{:#}", recipe.name);
@@ -29,8 +54,8 @@ fn main() -> std::io::Result<()> {
     Ok(())
 }
 
-fn chooser(recipes: &[Recipe]) -> Recipe {
+fn chooser(food: &[Food]) -> Food {
 
-    return recipes[0].clone();
+    return food[0].clone();
 
 }
