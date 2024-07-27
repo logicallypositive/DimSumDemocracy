36549be7478812e51ae9c8dc9f39e82d35d1ae56
MausVII <elanlocke@gmail.com>
2024-07-27 17:39:46 -0400

    big pp

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,27 @@
 // ++ priority based on older recency date
 use std::fs::File;
 use std::io::BufReader;
+use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
-fn main() -> std::io::Result<()> {
+#[derive(Serialize, Deserialize, Debug)]
+struct Recipe {
+    id: u32,
+    name: String,
+    vegan: bool,
+    recency: String
+}
 
-    println!("Deez");
+fn main() -> std::io::Result<()> {
 
     let file = File::open("data/recipes.json")?;
     let reader = BufReader::new(file);
 
-    let json_data: Value = serde_json::from_reader(reader)?;
+    let json_data: Vec<Recipe> = serde_json::from_reader(reader)?;
 
-    println!("{:#}", json_data);
+    for recipe in json_data.iter() {
+        println!("{:#}", recipe.name);
+    }
 
     Ok(())
 }
