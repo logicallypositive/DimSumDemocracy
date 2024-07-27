2a62fb493ccfd9f87eb11058876398ebbee44696
logically positive <brianizquierdo0229@gmail.com>
2024-07-27 01:25:42 -0400

    new default recipe and basic printing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
+// ++ priority based on older recency date
+use std::fs::File;
+use std::io::BufReader;
+use serde_json::Value;
+
+fn main() -> std::io::Result<()> {
+
     println!("Deez");
+
+    let file = File::open("data/recipes.json")?;
+    let reader = BufReader::new(file);
+
+    let json_data: Value = serde_json::from_reader(reader)?;
+
+    println!("{:#}", json_data);
+
+    Ok(())
 }
